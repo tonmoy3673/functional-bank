8096f85d4a61791a3d6891fba467ba705372dc8e
tonmoy <tonmoy3673@gmail.com>
2024-05-24 16:42:49 +0600

    :tada: set elementID function

--- apps.js
+++ apps.js
@@ -1,1 +1,13 @@
-console.log('connect');
+
+
+// =============== getElementByText===============//
+const textElement=(elementID)=>{
+    const elementValue=document.getElementById(elementID);
+    const valueParse=elementValue.innerText;
+    const value=parseFloat(valueParse);
+    console.log(value);
+};
+
+textElement('balance');
+
+// ================= getInputElementValue===============//
