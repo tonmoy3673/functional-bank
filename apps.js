0260b8ab5b18484fc09ab74c167e7074953df172
tonmoy <tonmoy3673@gmail.com>
2024-05-24 16:57:34 +0600

    implemented getDeposit and getWithdraw

--- apps.js
+++ apps.js
@@ -5,9 +5,32 @@ const textElement=(elementID)=>{
     const elementValue=document.getElementById(elementID);
     const valueParse=elementValue.innerText;
     const value=parseFloat(valueParse);
-    console.log(value);
+    elementValue.innerText='';
+    return value
 };
 
 textElement('balance');
 
 // ================= getInputElementValue===============//
+
+const getInputValue=(inputID)=>{
+    const inputValue=document.getElementById(inputID);
+    const inputParse=inputValue.value;
+    const input=parseFloat(inputParse);
+    inputValue.value='';
+    return input;
+
+
+
+};
+
+
+document.getElementById('deposit-btn').addEventListener('click',function(){
+   const newDepositAmount= getInputValue('deposit-input');
+   console.log(newDepositAmount);
+});
+
+document.getElementById('withdraw-btn').addEventListener('click',function(){
+  const withdrawAmount=  getInputValue('withdraw-input');
+  console.log(withdrawAmount);
+});
