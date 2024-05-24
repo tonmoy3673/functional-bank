647260ccef644f736c7c6c782d4b835c5c0a16aa
tonmoy <tonmoy3673@gmail.com>
2024-05-24 17:55:09 +0600

    :tada: integrated balance and deposit value

--- apps.js
+++ apps.js
@@ -5,7 +5,7 @@ const textElement=(elementID)=>{
     const elementValue=document.getElementById(elementID);
     const valueParse=elementValue.innerText;
     const value=parseFloat(valueParse);
-    elementValue.innerText='';
+    
     return value
 };
 
@@ -20,17 +20,34 @@ const getInputValue=(inputID)=>{
     inputValue.value='';
     return input;
 
-
-
 };
 
 
+// =============== Set Deposit =============//
 document.getElementById('deposit-btn').addEventListener('click',function(){
    const newDepositAmount= getInputValue('deposit-input');
-   console.log(newDepositAmount);
+   const previousDepositAmount=textElement('deposit');
+   const totalDeposit=(previousDepositAmount+newDepositAmount);
+   const getDepositValue=document.getElementById('deposit');
+   getDepositValue.innerText=totalDeposit;
+   const balanceValue=textElement('balance');
+   const balanceTotal=(balanceValue + newDepositAmount);
+   const balanceText=document.getElementById('balance')
+   balanceText.innerText=balanceTotal;
+   
 });
 
+// ================ Set Withdraw ===============//
 document.getElementById('withdraw-btn').addEventListener('click',function(){
-  const withdrawAmount=  getInputValue('withdraw-input');
-  console.log(withdrawAmount);
+  const newWithdrawAmount=  getInputValue('withdraw-input');
+  console.log(newWithdrawAmount);
+  const previousWithdrawAmount=textElement('withdraw');
+  console.log(previousWithdrawAmount);
+  const totalWithdraw=newWithdrawAmount+previousWithdrawAmount
+  const withdrawText=document.getElementById('withdraw');
+  withdrawText.innerText=totalWithdraw;
+
 });
+
+
+
